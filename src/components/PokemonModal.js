3ae5929dc79aa68c65c561b3fd8047fce5c2d036
Gabriel Arteaga <gabrielarteaga71@gmail.com>
2017-12-12 19:48:49 -0800

    initial commit

---
+++ src/components/PokemonModal.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Modal, Button} from 'react-bootstrap/lib/';
+import PokemonInfo from './PokemonInfo';
+
+const PokemonModal = ({closeModal, showModal, pokemon}) => {
+  return (
+    <div>
+        <Modal
+          show={showModal}
+          onHide={closeModal}
+          aria-labelledby="contained-modal-title"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title">{pokemon !== null ? pokemon.name : 'Loading...'}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            { pokemon !== null ? <PokemonInfo pokemon={pokemon} /> : null }
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={closeModal}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+    </div>
+  )
+}
+
+export default PokemonModal;
